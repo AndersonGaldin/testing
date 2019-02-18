082d411e6418a9d0c2aa83b76002933b94d1ab29
Anderson Galdino <andersongaldinox@gmail.com>
2019-02-18 00:33:06 -0300

    Testing application with redux with testings with jest and enzyme

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
-
 beforeEach(() => {
-  wrapped = mount(<CommentBox />);
+  wrapped = mount(
+    <Root>
+    <CommentBox />
+    </Root>
+  );
 });
 
 afterEach(() => {
@@ -15,4 +19,23 @@ afterEach(() => {
 it('has text area and button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: {value: 'new comment'}
+    });
+    wrapped.update();
+  });
+
+  it('has text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('when form is submitted, text are gets emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
