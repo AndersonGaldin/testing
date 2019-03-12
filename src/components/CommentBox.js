6db3af16eb5917a3a95c1f13973d7b87035fd63d
Anderson Galdino <andersongaldinox@gmail.com>
2019-03-12 08:42:07 -0300

    coverage testing

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -11,21 +11,27 @@ class CommentBox extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.saveComment(this.state.comment)
+    this.props.saveComment(this.state.comment);
     this.setState({comment: ''});
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a Comment</h4>
-        <textarea value={this.state.comment} onChange={this.handleChange} />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea value={this.state.comment} onChange={this.handleChange} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(
+  null,
+  actions
+)(CommentBox);
